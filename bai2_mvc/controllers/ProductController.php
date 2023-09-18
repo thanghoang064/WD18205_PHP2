670e7a50bfe9c8bbabea87ca1c4f6d30a393a407
ITPLUS Academy <thanghoang064@gmail.com>
2023-09-18 15:02:01 +0700

    update project

--- bai2_mvc/controllers/ProductController.php
+++ bai2_mvc/controllers/ProductController.php
@@ -1,8 +1,14 @@
 <?php
-
+require_once "models/Product.php";
 function listProduct() {
+    //tạo ra 1 biến hứng dữ liệu từ hàm của model
+    $products = getProduct();
+//    echo "<pre>";
+//    var_dump($products);
+//    die;
+    include_once "views/product/list.php";
     // lấy dữ liệu từ model ra controller và tạo 1 view danh sách đổ xuống view
-    echo "Đây là trang danh sách product";
+//    echo "Đây là trang danh sách product";
 }
 
 function addProduct() {
