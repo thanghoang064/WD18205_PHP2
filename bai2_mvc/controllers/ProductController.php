b4f00627224688d90aa891b3a849cbf2bd66829f
trungnt173 <trungnt173@fpt.edu.vn>
2023-09-27 15:43:20 +0700

    update project

--- bai2_mvc/controllers/ProductController.php
+++ bai2_mvc/controllers/ProductController.php
@@ -1,23 +1,32 @@
 <?php
 require_once "models/Product.php";
-function listProduct() {
-    //tạo ra 1 biến hứng dữ liệu từ hàm của model
-    $products = getProduct();
+class ProductController
+{
+   public function listProduct()
+    {
+        //tạo ra 1 biến hứng dữ liệu từ hàm của model
+        $product = new Product();
+        $products = $product->getProduct();
 //    echo "<pre>";
 //    var_dump($products);
 //    die;
-    include_once "views/product/list.php";
-    // lấy dữ liệu từ model ra controller và tạo 1 view danh sách đổ xuống view
+        include_once "views/product/list.php";
+        // lấy dữ liệu từ model ra controller và tạo 1 view danh sách đổ xuống view
 //    echo "Đây là trang danh sách product";
-}
+    }
 
-function addProduct() {
-    echo "Đây là trang thêm product";
-}
+   public function addProduct()
+    {
+        echo "Đây là trang thêm product";
+    }
 
-function editProduct() {
-    echo "Đây là trang sửa product";
-}
-function deleteProduct() {
-    echo "Đây là chức năng xóa product";
+   public function editProduct()
+    {
+        echo "Đây là trang sửa product";
+    }
+
+   public function deleteProduct()
+    {
+        echo "Đây là chức năng xóa product";
+    }
 }
